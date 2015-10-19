bb6035d73c0fc5f679ed6dac4eef386bac72e010
Jose Manuel Peso <jmpeso@gmail.com>
2015-10-19 08:12:59 +0200

    custom main sass file included

--- app/panel/main.js
+++ app/panel/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Sidebar from './components/sidebar.jsx';
 import Content from './components/content.jsx';
-
+import './scss/main.scss'
 main();
 
 function main() {
