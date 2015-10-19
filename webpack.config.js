2cda61c0ba411df352e7e1ec83d622b8a3bdd2dc
Jose Manuel Peso <jmpeso@gmail.com>
2015-10-19 07:51:18 +0200

    sass bootstrap webpack capable

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
 var path = require('path');
 var config = {
-  entry: path.resolve(__dirname, 'app/panel/main.js'),
+  entry: [
+     path.resolve(__dirname, 'app/panel/main.js'),
+    'bootstrap-sass!./app/common/bootstrap-sass.config.js'
+  ],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
-      loader: 'babel' // The module to load. "babel" is short for "babel-loader"
-    }]
+    loaders: [
+      //JSX
+      { test: /\.jsx?$/, loader: 'babel' },
+      { test: /\.scss$/, loader: "style!css!sass?outputStyle=expanded" },
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" }
+    ]
   }
 };
 
